b2a1a74e9377eff8a3a6d0a87f29e0d4af7b834e
b0dhidharma <78811746+b0dhidharma@users.noreply.github.com>
2022-03-21 23:58:42 -0300

    feat: imports app from monorepo (#2)

--- eth-sdk/config.ts
+++ eth-sdk/config.ts
@@ -1,5 +1,10 @@
 import { defineConfig } from '@dethcrypto/eth-sdk';
 
 export default defineConfig({
-  contracts: {},
+  contracts: {
+    mainnet: {
+      curveFi: '0x98638FAcf9a3865cd033F36548713183f6996122',
+      yvBoostVault: '0x9d409a0A012CFbA9B15F6D4B36Ac57A46966Ab9a',
+    },
+  },
 });
